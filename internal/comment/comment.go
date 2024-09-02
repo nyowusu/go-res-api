6cee56821266bc207560bc84b74ea18d1ebee909
Yaw Boakye-Owusu <yaw.owusu@hotmail.com>
2024-09-03 00:30:59 +0100

    perf: accept interfaces and return structs

---
+++ internal/comment/comment.go
@@ -0,0 +1,51 @@
+package comment
+
+import (
+	"context"
+	"fmt"
+)
+
+// Comment - a representation of the comment
+// structure for our service
+type Comment struct {
+	ID     string
+	Slug   string
+	Body   string
+	Author string
+}
+
+// Store - this interface defines all of the
+// methods that our service needs
+// in order to operate
+type Store interface {
+	GetComment(context.Context, string) (Comment, error)
+}
+
+// Service - is a the struct on which all our
+// logic will be built on top of
+type Service struct {
+	Store Store
+}
+
+// NewService - returns a pointer to a new
+// service
+// NOTE: Accept interfaces, Return Structs
+func NewService(store Store) *Service {
+	return &Service{
+		Store: store,
+	}
+}
+
+func (s *Service) GetComment(ctx context.Context, id string) (Comment, error) {
+	fmt.Println("Retrieving a comment")
+
+	// reaches to the repository layer to retrieve a comment
+	cmt, err := s.Store.GetComment(ctx, id)
+
+	if err != nil {
+		fmt.Println(err)
+		return Comment{}, err
+	}
+
+	return cmt, nil
+}
